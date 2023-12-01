ce49085ffda6f0f6b5eb57abcb34157f2c292b56
Kevin Jose <krsj1234@gmail.com>
2023-12-01 20:25:56 +0530

    add some minor fix for article page

--- article/article.js
+++ article/article.js
@@ -1,6 +1,10 @@
 const headerCont = document.getElementById('article-nav');
 const contents = document.querySelectorAll('.content');
 const titlePage = document.querySelector('.title-grouping');
+const cutTheBs = document.getElementById('cutTheBs');
+const elaborate = document.getElementById('elaborate');
+const elaborateContent = document.getElementById('detailed-content');
+const summarizedContent = document.getElementById('summarized-content');
 
 
 const mainOpt = {
@@ -36,3 +40,25 @@ contents.forEach(content => {
     mainPageObserver.observe(content);
 });
 titleObserver.observe(titlePage);
+
+function handleBsButton(e)
+{
+    console.log(e);
+    elaborateContent.classList.add('display-none');
+    summarizedContent.classList.remove('display-none');
+    cutTheBs.classList.add('display-none');
+    elaborate.classList.remove('display-none');
+}
+
+function handleElaborateButton(e)
+{
+    console.log(e);
+    summarizedContent.classList.add('display-none');
+    elaborateContent.classList.remove('display-none');
+    elaborate.classList.add('display-none');
+    cutTheBs.classList.remove('display-none');
+}
+
+
+if (cutTheBs)cutTheBs.addEventListener('click', handleBsButton);
+if (elaborate)elaborate.addEventListener('click', handleElaborateButton);
