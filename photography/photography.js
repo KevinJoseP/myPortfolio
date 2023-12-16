35be66e332f7422575dda370f0a01faf7b62a103
Kevin Jose <krsj1234@gmail.com>
2023-12-17 01:33:38 +0530

    fix the bottom nav bar not appearing issue in case of gallery

--- photography/photography.js
+++ photography/photography.js
@@ -11,18 +11,21 @@ const back2TopObserverMobile = new IntersectionObserver (handleBack2TopMobile, {
 });
 const shuffleBtn = document.querySelector('.shuffle');
 
+
 const MAX_SMILES_PHOTO_COUNT = 63;
 const MAX_GALLERY_PHOTO_COUNT = 66;
 const SMILE_URL = "./smiles/smiles";
 const GALLERY_URL = "./gallery/gallery"
 
 const mediaQuery = window.matchMedia('(max-width: 40em)');
+let isCurrentScreenSmiles = false;
 let isMobile = mediaQuery.matches;
 mediaQuery.addEventListener('change', handleMediaChange);
 
 function handleMediaChange(e)
 {
     let screenOldValue = isMobile;
+    //console.log("hanldeMediaChange");
     if (e.matches)
     {
         isMobile = true;
@@ -41,18 +44,33 @@ function handleMediaChange(e)
 
 function triggerMediaChangeActions()
 {
+    //console.log("triggerMediaChangeActions");
     if(isMobile)
     {
         deleteHackDiv();
+        addBottomPanelObserverForMobile();
     }
     else
     {
         addBackToTopHack();
+        removeBottomPanelObserverForLargeScreens();
     }
-
+    deleteBottomNavbar();
+    addBottomNavBar();
 }
 
 
+function getCurrentGallery()
+{
+    if (navBarContHeader.classList.contains('type-smile'))
+    {
+        return smilesGallery;
+    }
+    else
+    {
+        return photoGallery;
+    }
+}
 
 function deleteHackDiv()
 {
@@ -71,33 +89,43 @@ function deleteHackDiv()
 
 function addBackToTopHack()
 {
-    let galleryCnt;
-    if (navBarContHeader.classList.contains('type-smile'))
-    {
-        galleryCnt = smilesGallery;
-    }
-    else
-    {
-       galleryCnt = photoGallery;
-    }
-    
+    let galleryCnt = getCurrentGallery();
     const backToTopBtnHackdiv = document.createElement('div');
     backToTopBtnHackdiv.classList.add('backtotophack');
     back2TopObserver.observe(backToTopBtnHackdiv);
     galleryCnt.insertBefore(backToTopBtnHackdiv, galleryCnt.children[1]);
 }
 
-function addBottomNavBar(galleryCnt)
+function addBottomNavBar()
 {
+    let currGalleryCnt = getCurrentGallery();
     const bottomNavBar = document.createElement('div');
     bottomNavBar.classList.add('bottom-control-panel');
 
     addBackToTopBtn(bottomNavBar);
     addShuffleBtn(bottomNavBar);
-    galleryCnt.appendChild(bottomNavBar);
+    currGalleryCnt.appendChild(bottomNavBar);
+    //console.log("added bottom nav")
 }
 
-function addShuffleBtn(galleryCnt)
+function deleteBottomNavbar()
+{
+    if (navBarContHeader.classList.contains('type-smile'))
+    {
+        bottomNavBar = document.querySelector('#smiles-intro .bottom-control-panel');
+        
+    }
+    else
+    {
+        bottomNavBar = document.querySelector('#photography-intro .bottom-control-panel');
+    }
+    //console.log(bottomNavBar);
+    let currGallery = getCurrentGallery();
+    currGallery.removeChild(bottomNavBar);
+    //console.log("deletedBottomNavBar");
+}
+
+function addShuffleBtn(navCnt)
 {
     var a = document.createElement('a');
     a.classList.add('shuffle');
@@ -111,10 +139,10 @@ function addShuffleBtn(galleryCnt)
     a.appendChild(img);
     a.addEventListener('click', handleShuffle);
 
-    galleryCnt.appendChild(a);
+    navCnt.appendChild(a);
 }
 
-function addBackToTopBtn(galleryCnt)
+function addBackToTopBtn(navCnt)
 {
     // Create the anchor element
     var a = document.createElement('a');
@@ -129,20 +157,22 @@ function addBackToTopBtn(galleryCnt)
     // Append the image and the description div to the anchor
     a.appendChild(img);
 
-    galleryCnt.appendChild(a);
+    navCnt.appendChild(a);
 }
 
 
-function fillInPhotos(isSmiles)
+function fillInPhotos()
 {
     let currMaxCount = 0;
-    if (isSmiles)
+    let currGallery;
+    if (isCurrentScreenSmiles)
     {
         currMaxCount = MAX_SMILES_PHOTO_COUNT;
         currUrl = SMILE_URL;
         photoGallery.innerHTML = '';
         smilesGallery.innerHTML = '';
         navBarContHeader.classList.add('type-smile');
+        currGallery = smilesGallery;
     }
     else
     {
@@ -151,6 +181,7 @@ function fillInPhotos(isSmiles)
         photoGallery.innerHTML = '';
         smilesGallery.innerHTML = '';
         navBarContHeader.classList.remove('type-smile');
+        currGallery = photoGallery;
 
     }
 
@@ -163,45 +194,41 @@ function fillInPhotos(isSmiles)
         galleryImg.src = tempUrl;
         photoCont.classList.add('photo-cont');
         photoCont.appendChild(galleryImg);
-        if (isSmiles)
-        {
-            smilesGallery.appendChild(photoCont);
-        }
-        else
-        {
-            photoGallery.appendChild(photoCont);
-        }
+        currGallery.appendChild(photoCont);
     }
 
-    if(isSmiles)
+    addBottomNavBar();
+    if (!isMobile)
     {
-        addBottomNavBar(smilesGallery);
+        addBackToTopHack();
     }
     else
-    { 
-        addBottomNavBar(photoGallery);
+    {
+        addBottomPanelObserverForMobile();
     }
-    if (!isSmiles)addBackToTopHack();
 }
 
 function handleChangeToGallery()
 {
     smilesSect.classList.add('display-none');
     gallerySect.classList.remove('display-none');
-    fillInPhotos(false);
+    isCurrentScreenSmiles = false;
+    fillInPhotos();
 }
 
 function handleChangeToSmile()
 {
     gallerySect.classList.add('display-none');
     smilesSect.classList.remove('display-none');
-    fillInPhotos(true);
+    isCurrentScreenSmiles = true;
+    fillInPhotos();
 }
 
 
 function handleBack2Top(entries, back2TopObserver)
 {
     entries.forEach(entry => {
+        // //console.log("handleBack2top")
         const backToTopBtn = document.querySelector('.back-to-top-photo');
         if (entry.isIntersecting)
         {
@@ -217,6 +244,7 @@ function handleBack2Top(entries, back2TopObserver)
 function handleBack2TopMobile(entries, back2TopObserverMobile)
 {
     entries.forEach(entry => {
+        // //console.log("handleBack2topMobile")
         let bottomNavBar;
         if (navBarContHeader.classList.contains('type-smile'))
         {
@@ -227,43 +255,40 @@ function handleBack2TopMobile(entries, back2TopObserverMobile)
         {
             bottomNavBar = document.querySelector('#photography-intro .bottom-control-panel');
         }
-
         if (!entry.isIntersecting)
         {
             bottomNavBar.classList.remove('display-none');
-            console.log("non intersecting");
         }
         else
         {
             bottomNavBar.classList.add('display-none');
-            console.log("intersecting")
         }
     });
 }
 
 function handleShuffle(e)
 {
+    if (!isMobile)deleteHackDiv();
+    deleteBottomNavbar();
+    let currGallery;
     if (navBarContHeader.classList.contains('type-smile'))
     {
-        deleteHackDiv();
-        const divs = Array.prototype.slice.call(smilesGallery.children);
-        while (divs.length) {
-            smilesGallery.appendChild(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-        }    
+        currGallery = smilesGallery;
     }
     else
     {
-        deleteHackDiv();
-        const divs = Array.prototype.slice.call(photoGallery.children);
-        while (divs.length) {
-            photoGallery.appendChild(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-        }
+        currGallery = photoGallery;
+    }
+    const divs = Array.prototype.slice.call(currGallery.children);
+    while (divs.length) {
+        currGallery.appendChild(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
     }
     if (!isMobile)addBackToTopHack();
+    addBottomNavBar(currGallery);
     
 }
 
-function addBottomPanelObserver()
+function addBottomPanelObserverForMobile()
 {
     if (navBarContHeader.classList.contains('type-smile'))
     {
@@ -275,17 +300,29 @@ function addBottomPanelObserver()
         const introSection = document.querySelector('#photography-intro .descr-section-title');
         back2TopObserverMobile.observe(introSection);
     }
+
 }
+
+function removeBottomPanelObserverForLargeScreens()
+{
+    if (navBarContHeader.classList.contains('type-smile'))
+    {
+        const introSection = document.querySelector('#smiles-intro .descr-section-title');
+        back2TopObserverMobile.unobserve(introSection);
+    }
+    else
+    {
+        const introSection = document.querySelector('#photography-intro .descr-section-title');
+        back2TopObserverMobile.unobserve(introSection);
+    }
+}
+
 
 function init()
 {
-    fillInPhotos(false);
+    fillInPhotos();
     if (myGalleryBtn)myGalleryBtn.addEventListener('click', handleChangeToGallery);
     if (smileSectBtn)smileSectBtn.addEventListener('click', handleChangeToSmile);
-    if (isMobile)
-    {
-        addBottomPanelObserver();
-    }
 }
 
 init();
